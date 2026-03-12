693013334dc2e1b6372f271af7af8e7a68544e30
Jasjeet Singh Babra <jasjeetsinghbabra123@gmail.com>
2026-03-12 16:09:37 +0530

    restart

--- task-1-complexity-analyzer/my-app/src/main.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './SmartComplexityAnalyzer'
-
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
